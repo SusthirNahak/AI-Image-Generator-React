488337c94ccbda015439f7a6fefec9d65779b388
susthir <susthir19march@gmail.com>
2024-05-08 12:46:52 +0530

    environment varible setup

--- ai_img_generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ ai_img_generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,24 +1,29 @@
 import React, { useRef, useState } from 'react'
 import './ImageGenerator.css'
 import def_img from '../../assets/medium.webp'
-export default function ImageGenerator() {
+const apiKey = process.env.api;
+const apiUrl = process.env.url;
 
-    const [image_url,setImage_url] =useState('/');
+const ImageGenerator = ()=> {
+
+    const [image_url,setImage_url] =useState("/");
     let inputRef = useRef(null);
 
-    const imageGenerator = async()=>{
+    const [loading,setLoading] =useState(false)
+    const imageGenerator = async () =>{
         if(inputRef.current.value === ""){
             return 0;
         }
+        setLoading(true);
         const responce = await fetch(
-            // "https://api.openai.com/v1/images/generations",
+            "{apiUrl}",
             {
                 method:"POST",
                 headers:{
                     "content-Type":"application/json",
                     Authorization:
-                    "Bearer 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",
-                    "User-Agent":"Chrome",
+                    "Bearer {apiKey}",
+                    "User-Agent":"Chrome"
                 },
                 body:JSON.stringify({
                     prompt:`${inputRef.current.value}`,
@@ -30,7 +35,9 @@ export default function ImageGenerator() {
 
         );
         let data =await responce.json();
-        console.log(data);
+        let data_array = data.data;
+        setImage_url(data_array[0].url);
+        setLoading(false);
             }
 
   return (
@@ -40,6 +47,10 @@ export default function ImageGenerator() {
     <div className="image">
         <img src={image_url==='/'?def_img:image_url} alt="" />
     </div>
+    <div className='loading'>
+        <div className={loading?"loading-bar-full":"loading-bar"}></div>
+        <div className={loading?"loading-text":"display-none"}>Loading....</div>
+    </div>
     </div>
     <div className="search-box">
     <input type="text" ref={inputRef} className='search-input' placeholder='Describe what you want to see' />
@@ -48,3 +59,4 @@ export default function ImageGenerator() {
     </div>
   )
 }
+export default ImageGenerator
